bd91e5eb4ca818a50cf278e0d0ff89fd577c25ef
chenk <hen.keinan@gmail.com>
2022-12-20 08:53:29 +0200

    feat: node info job
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,35 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-  Use:   "node-info",
-  Short: "k8s-Node-Info extract file system info from cluster Node",
-  Long: `A tool which provide a way to extract file system info fro node cluster based on pre-define commands`,
-  Run: func(cmd *cobra.Command, args []string) {
-    // Do Stuff Here
-  },
+	Use:   "node-info",
+	Short: "k8s-Node-Info extract file system info from cluster Node",
+	Long:  `A tool which provide a way to extract file system info fro node cluster based on pre-define commands`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var stdout bytes.Buffer
+		var stderr bytes.Buffer
+		cm := exec.Command("stat", "-c", "%U:%G", "/etc/kubernetes/manifests/kube-controller-manager.yaml")
+		cm.Stdout = &stdout
+		cm.Stderr = &stderr
+		err := cm.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Print(stdout.String())
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
